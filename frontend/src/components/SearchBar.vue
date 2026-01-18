2ebd9266dda71c3975db81444119eb50d6fd0508
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2026-01-18 11:53:56 -0500

    added search component

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <input
+        type="text"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        placeholder="Search for breweries..."
+    />
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    props: {
+        modelValue: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
