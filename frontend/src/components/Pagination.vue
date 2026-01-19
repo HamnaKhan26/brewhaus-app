30d9c1e2d8523d3fa261aba41ac280a3f65513a1
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2026-01-19 09:45:34 -0500

    added pagination component

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pagination">
+        <button :disabled="currentPage === 1" @click="changePage(currentPage - 1)">Prev</button>
+        <span>Page {{ currentPage }}</span>
+        <button :disabled="!hasNextPage" @click="changePage(currentPage + 1)">Next</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['currentPage', 'hasNextPage'],
+    emits: ['page-changed'],
+    methods: {
+        changePage(page) {
+            console.log('Emitting page:', page);
+            this.$emit('page-changed', page);
+        }
+    }
+};
+</script>
